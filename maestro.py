afe7e96b00b7d8a68c50d5efe28bfbf20b7db547
ChancellorO <chancellororichey@gmail.com>
2022-11-07 21:48:27 -0800

    Added simple framework

--- maestro.py
+++ maestro.py
@@ -1,3 +1,22 @@
 #Main file
 from astropy import units as u
 
+
+class Maestro:
+    def __init__(self):
+        pass
+    pass
+
+def main():
+    run()
+    pass
+
+def run():
+    while(True):
+        pass
+    pass
+
+
+
+if __name__ == "__main__":
+    main()
